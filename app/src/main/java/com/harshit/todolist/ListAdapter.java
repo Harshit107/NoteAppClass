2ebf4596691ac58cac9e759958e0ff87745c2472
Harshit Keshari <harshit107.in@gmail.com>
2021-02-28 22:55:36 +0530

    Firebase update

--- app/src/main/java/com/harshit/todolist/ListAdapter.java
+++ app/src/main/java/com/harshit/todolist/ListAdapter.java
@@ -1,6 +1,8 @@
 package com.harshit.todolist;
 
+import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,19 +18,20 @@ import java.util.ArrayList;
 
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder>{
 
+    private static final String TAG = "AdapterClass";
     Context context;
     ArrayList<MyList> arrayList;
+    Activity activity;
 
-    public ListAdapter(Context context, ArrayList<MyList> arrayList) {
+    public ListAdapter(Context context, ArrayList<MyList> arrayList, MainActivity activity) {
         this.context = context;
         this.arrayList = arrayList;
+        this.activity = activity;
     }
-    
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
         View view = LayoutInflater.from(context).inflate(R.layout.item_layout, parent, false);
         return new MyViewHolder(view);
     }
@@ -42,7 +45,20 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder>{
             public void onClick(View view) {
 //                Toast.makeText(context, holder.getAdapterPosition()+"",Toast.LENGTH_SHORT).show();
                 arrayList.remove(holder.getAdapterPosition());
+                removeFromMain(holder.getAdapterPosition());
                 notifyDataSetChanged();
+            }
+        });
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                String text = arrayList.get(holder.getAdapterPosition()).getText();
+                arrayList.remove(holder.getAdapterPosition());
+                removeFromMain(holder.getAdapterPosition());
+                notifyDataSetChanged();
+                ((MainActivity)activity).editView(text);
+
+                return false;
             }
         });
 
@@ -70,5 +86,10 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder>{
         this.arrayList = lists;
         notifyDataSetChanged();
     }
+
+    public void removeFromMain(int pos) {
+        ((MainActivity)activity).removeItem(pos);
+    }
+
 
 }
--- app/src/main/java/com/harshit/todolist/MainActivity.java
+++ app/src/main/java/com/harshit/todolist/MainActivity.java
@@ -1,25 +1,47 @@
 package com.harshit.todolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "Main";
+    public static final String SHARED = "users";
     RecyclerView recyclerView;
     ArrayList<MyList> lists = new ArrayList<>();
     ListAdapter listAdapter;
     EditText itemEt;
     Button itemBt;
+
+    DatabaseReference databaseReference;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +50,39 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycle);
         itemBt = findViewById(R.id.addBt);
         itemEt = findViewById(R.id.addEt);
-        lists.add(new MyList("Hello"));
+        databaseReference =  FirebaseDatabase.getInstance().getReference().child("users");
 
 
-        listAdapter = new ListAdapter(getApplicationContext(), lists);
+        listAdapter = new ListAdapter(getApplicationContext(), lists, this);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(listAdapter);
+
+        //filter
+        itemEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String changed = editable.toString();
+                ArrayList<MyList> myNewList = new ArrayList<>();
+                for(int i = 0; i<lists.size(); i++ ) {
+                    if(lists.get(i).getText().contains(changed)) {
+                     myNewList.add(lists.get(i));
+                    }
+                }
+                listAdapter.renewItem(myNewList);
+            }
+        });
+
+        //list => String , Integer list.contains(
+        //list => MyList-> string )
+
 
         itemBt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,10 +93,26 @@ public class MainActivity extends AppCompatActivity {
                     itemEt.requestFocus();
                     return;
                 }
-                lists.add(new MyList(value));
-                listAdapter.renewItem(lists);
-                itemEt.setText("");
-                Log.d(TAG,lists.toString());
+                //current times in mili - use as id
+                String id = System.currentTimeMillis()+"";
+
+                //ussing id
+                databaseReference.child(id).setValue(value)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                               addToRecyclerView(value,id);
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getApplicationContext(), e.getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+
+
             }
         });
 
@@ -56,5 +121,118 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        /************************************************/
+
+//        FirebaseDatabase mRef = FirebaseDatabase.getInstance();
+//        DatabaseReference dRef = mRef.getReference("status");
+
+//        dRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot data) {
+//
+//                Log.d(TAG, data.getValue().toString());
+//
+//            }
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//                Log.d(TAG, "Error" +error.toString());
+//            }
+//        });
+//        dRef.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot data) {
+//
+//                String key = data.getKey();
+//                Log.d(TAG, "Key =  " +key);
+//                Log.d(TAG, data.getValue().toString());
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+
+
+//        dRef.child("123456").child("age").setValue("21");
+//        dRef.child("123456").child("name").setValue("Harshit");
+//        dRef.child("123456").child("gender").setValue("M");
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot data) {
+                if(data.exists()){
+                    lists.clear();
+                    for (DataSnapshot myData: data.getChildren()) {
+                        // TODO: handle the post
+                        String key = myData.getKey().toString();
+                        String value = myData.getValue().toString();
+                        lists.add(new MyList(value,key));
+
+                    }
+                    listAdapter.renewItem(lists);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
+
+    public void addToRecyclerView(String value, String id) {
+        lists.add(new MyList(value,id));
+        listAdapter.renewItem(lists);
+        itemEt.setText("");
+        Log.d(TAG,lists.toString());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+//        SharedPreferences.Editor editor =  getSharedPreferences(SHARED,MODE_PRIVATE).edit();
+//        editor.putString("list",lists.toString());
+//        editor.apply();
+
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+         SharedPreferences s = getSharedPreferences(SHARED,MODE_PRIVATE);
+         String myList = s.getString("list","-1");
+         //left------------------------------------------------------------------------
+    }
+
+    public void editView( String text) {
+        itemEt.setText(text);
+
+    }
+
+
+    public void removeItem(int pos) {
+        String id = lists.get(pos).getId();
+        databaseReference.child(id).removeValue().addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        lists.remove(pos);
+                    }
+        });
+
+    }
+
+
+
+
 }
